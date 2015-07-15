9526f1333109c6be9634cd22d5a01c329d500fc5
mmd-work <martin.dowie@selex-es.com>
2015-07-15 11:52:21 +0100

    Create E6_AAA_Example_1_Main.h

---
+++ E6_AAA_Example_1_Main.h
@@ -0,0 +1,23 @@
+#ifndef E6_AAA_Example_1_Main_H
+#define E6_AAA_Example_1_Main_H
+
+//## package E_Test::E6_SemanticAnalysis::E6_AAA_Example_1
+
+//## class E6_AAA_Example_1_Main
+class E6_AAA_Example_1_Main {
+    ////    Constructors and destructors    ////
+    
+public :
+
+    //## operation E6_AAA_Example_1_Main()
+    E6_AAA_Example_1_Main();
+    
+    ////    Operations    ////
+
+private :
+
+    //## operation divide(int,int) const
+    int divide(int lhs, int rhs) const;
+};
+
+#endif
